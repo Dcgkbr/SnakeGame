8511e026b0d03a916b15c8e77bef968ad315e02d
Dcgkbr <azomingo47@gmail.com>
2024-12-30 01:10:24 +0300

    Define variables, constants, and directions for game setup

--- snakegame.cpp
+++ snakegame.cpp
@@ -3,3 +3,12 @@
 #include <ctime>
 #include <ncurses.h>
 #include <vector>
+bool gameOver;
+const int width = 20;
+const int height = 10;
+enum eDirection { STOP = 0, LEFT, RIGHT, UP, DOWN };
+eDirection dir;
+int x, y, foodx, foody, score;
+vector<int> tailX;
+vector<int> tailY;
+int tailLength;
